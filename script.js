8ec2bc4c75a832fb8e50de7d6e427b35b444c066
AF Tamim <itamim345@gmail.com>
2022-06-24 07:00:19 +0530

    updated css & js

--- script.js
+++ script.js
@@ -12,8 +12,32 @@ let displayCountries = countries =>{
         //console.log(country.name.common);
         let div = document.createElement('div');
         div.classList.add('single-country');
-        div.innerHTML = `<h3>${country.name.common}</h3>
-                        <button>Get Details</button> 
+        div.innerHTML = `<h3 class="text-warning">${country.name.common}</h3>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Get Details</button> 
+                        <!-- Modal -->
+                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                <p>Name : kiamson jarfred</p>
+                                <p>Name : kiamson jarfred</p>
+                                <p>Name : kiamson jarfred</p>
+                                <p>Name : kiamson jarfred</p>
+                                <p>Name : kiamson jarfred</p>
+                                <p>Name : kiamson jarfred</p>
+                                <p>Name : kiamson jarfred</p>
+                                <p>Name : kiamson jarfred</p>
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
                         `;
         wrapper.appendChild(div)
     });
