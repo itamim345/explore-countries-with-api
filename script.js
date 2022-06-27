7d9b46d517ff0ae4aec3296b4b4e0e633208bdd8
AF Tamim <itamim345@gmail.com>
2022-06-27 06:45:12 +0530

    modified css & js to fix some issue

--- script.js
+++ script.js
@@ -6,39 +6,40 @@ let getCountries = () => {
 getCountries()
 
 let displayCountries = countries =>{
-    console.log(countries);
+    //console.log(countries);
     let wrapper = document.getElementById('countries-wrapper');
     countries.forEach(country => {
         //console.log(country.name.common);
         let div = document.createElement('div');
         div.classList.add('single-country');
         div.innerHTML = `<h3 class="text-warning">${country.name.common}</h3>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Get Details</button> 
-                        <!-- Modal -->
-                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                <p>Name : kiamson jarfred</p>
-                                <p>Name : kiamson jarfred</p>
-                                <p>Name : kiamson jarfred</p>
-                                <p>Name : kiamson jarfred</p>
-                                <p>Name : kiamson jarfred</p>
-                                <p>Name : kiamson jarfred</p>
-                                <p>Name : kiamson jarfred</p>
-                                <p>Name : kiamson jarfred</p>
-                                </div>
-                                <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
+                        <button onclick="getSingleCountry('${country.name.common}')" type="button" class="btn btn-primary">Get Details</button> 
                         `;
         wrapper.appendChild(div)
     });
+}
+
+let getSingleCountry = (cname) =>{
+    let url = `https://restcountries.com/v3.1/name/${cname}`
+    fetch (url)
+    .then (resp => resp.json())
+    .then(data => detailsInfo(data))
+}
+function detailsInfo (data){
+    console.log(data);
+    let wrap = document.getElementById('single-country-details');
+    wrap.innerHTML = `<div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="${data[0].flags.png}" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title text-center">${data[0].name.common}</h5>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Capital : - ${data[0].capital}</li>
+                            <li class="list-group-item">Population: ${data[0].population}</li>
+                            <li class="list-group-item">Region: ${data[0].region}</li>
+                            <li class="list-group-item">Start of Week: ${data[0].startOfWeek}</li>
+                            <li class="list-group-item">Start of Week: ${data[0].timezones}</li>
+                        </ul>
+                    </div>
+                        `;
 }
